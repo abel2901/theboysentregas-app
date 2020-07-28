b925a7dd1646b3685a19ba3e41dcb86a921974be
abelaraujo <abel2901@gmail.com>
2020-07-28 10:46:17 -0300

    First commit

---
+++ app/src/main/java/com/br/theboysentregasapp/Message.java
@@ -0,0 +1,42 @@
+package com.br.theboysentregasapp;
+
+public class Message {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getToId() {
+        return toId;
+    }
+
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+
+    private long timestamp;
+    private String fromId;
+    private String toId;
+}
---
+++ app/src/main/java/com/br/theboysentregasapp/RegisterActivity.java
@@ -0,0 +1,183 @@
+package com.br.theboysentregasapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mEditUsername;
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private Button mBtnSelectedPhoto;
+    private Uri mSelectedUri;
+    private ImageView mImgPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditUsername = findViewById(R.id.edit_username);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mBtnSelectedPhoto = findViewById(R.id.btn_selected_photo);
+        mImgPhoto = findViewById(R.id.image_photo);
+
+        mBtnSelectedPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPhoto();
+            }
+        });
+
+        getSupportActionBar().setTitle("Registrar");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && data != null) {
+            mSelectedUri = data.getData();
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mSelectedUri);
+                mImgPhoto.setImageDrawable(new BitmapDrawable(bitmap));
+                mBtnSelectedPhoto.setAlpha(0);
+            } catch (IOException e) {
+
+            }
+
+        }
+    }
+
+    private void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+
+    }
+
+    private void createUser() {
+        String name = mEditUsername.getText().toString();
+        String email = mEditEmail.getText().toString();
+        String password = mEditPassword.getText().toString();
+
+        if (name == null || name.isEmpty() || email == null || email.isEmpty() || password == null || password.isEmpty()) {
+            Toast.makeText(this, "Nome, senha e email devem ser preenchidos", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i("Teste", task.getResult().getUser().getUid());
+
+                            saveUserInFirebase();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Teste", e.getMessage());
+                        Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }
+
+    private void saveUserInFirebase() {
+        String filename = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/images/" + filename);
+        ref.putFile(mSelectedUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.i("Teste", uri.toString());
+
+                                String uid = FirebaseAuth.getInstance().getUid();
+                                String username = mEditUsername.getText().toString();
+                                String profileUrl = uri.toString();
+
+                                User user = new User(uid, username, profileUrl);
+
+                                FirebaseFirestore.getInstance().collection("users")
+                                        .document(uid)
+                                        .set(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+
+                                                Intent intent = new Intent(RegisterActivity.this, MessagesActivity.class);
+
+                                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                                                startActivity(intent);
+
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.i("Teste", e.getMessage());
+                                            }
+                                        });
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Teste", e.getMessage(), e);
+                    }
+                });
+    }
+
+}
